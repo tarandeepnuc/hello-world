e29092b5d3deb454c882002e812c073a6130d627
Tarandeep <tarandeep1982@yahoo.com>
2017-08-27 15:31:26 -0400

    File Modified

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		for(int i = 0; i< 10; i++) {
-			System.out.println("Hello To number ");
+			System.out.println("Hello To numbers ");
 			System.out.println(i);
 		}
 	}
